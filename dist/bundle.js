2ac33d4520c94ec57f8c944cf5b38d5fc9558e94
TaTia <tatyanakoryachkina@yandex.ru>
2023-07-10 18:24:23 +0300

    did the 2nd task of the 4th hw

--- dist/bundle.js
+++ dist/bundle.js
@@ -63,145 +63,272 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ "./style.css");
 
 
-const levels = [{ level: 1 }, { level: 2 }, { level: 3 }];
-
-const gameLevelEl = document.querySelector(".card-game");
+const gameLevelEl = document.querySelector(".deck");
 
 const renderCardGame = () => {
-  const cardGameHTML = `
-        <div class="container center">
-            <form class="game">
-                <h3 class="title">Выбери <br>
-                сложность</h3>
-                <div class="levels">
-                    <label for="1"><div class="level level-first"><input id="1" name="item" value="1" type="radio" class="level-input">1</div></label>
-                    <label for="2"><div class="level level-second"><input id="2" name="item" value="2" type="radio" class="level-input">2</div></label>
-                    <label for="3"><div class="level level-third"><input id="3" name="item" value="3" type="radio" class="level-input">3</div></label>
-                </div>
-                <button class="start-button" type="submit">Старт</button>
-            </form>
-        </div>`;
+    topGame.innerHTML = "";
 
-  gameLevelEl.innerHTML = cardGameHTML;
+    const cardGameHTML = `
+        <form class="game">
+            <h1 class="title">Выбери сложность</h1>
+            <div class="levels">
 
-  const levelEl = document.querySelector(".game");
-  const startButton = document.querySelector(".start-button");
+                <label for="1">
+                    <div class="level">
+                        <input class="level-input" id="1" name="level" value="1" type="radio">
+                        1
+                    </div>
+                </label>
 
-  const levelButtons = document.querySelectorAll('input[type="radio"]');
-  levelButtons.forEach((levelButton) => {
-    levelButton.addEventListener("change", () => {
-      levelButtons.forEach((button) => {
-        if (button !== levelButton) {
-          button.parentElement.classList.remove("chosen");
-        }
-      });
-      levelButton.parentElement.classList.add("chosen");
-      currentLevel = levelButton;
-    });
-  });
+                <label for="2">
+                    <div class="level">
+                        <input class="level-input" id="2" name="level" value="2" type="radio">
+                        2
+                    </div>
+                </label>
 
-  levelEl.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const chosenLevel = document.querySelector('input[type="radio"]:checked');
-    if (chosenLevel) {
-      currentLevel = chosenLevel.value;
-    } else {
-      alert("Выберите уровень");
+                <label for="3">
+                    <div class="level">
+                        <input class="level-input" id="3" name="level" value="3" type="radio">
+                        3
+                    </div>
+                </label>
+
+            </div>
+            <button type="submit" class="start-button" disabled>Старт</button>
+        </form>
+        `;
+
+    gameLevelEl.innerHTML = cardGameHTML;
+
+    const levelEl = document.querySelector(".levels");
+    const startButton = document.querySelector(".start-button");
+
+    
+    const buttonsLevel = document.querySelectorAll(".level");
+    for (const button of buttonsLevel) {
+        button.addEventListener("click", function () {
+            for (const button of buttonsLevel) {
+                button.classList.remove("chosen");
+            }
+            this.classList.add("chosen");
+        });
     }
-  });
 
-  function chooseLevel() {
-    startButton.addEventListener("click", () => {
-      let level = document.querySelector('input[type="radio"]:checked').value;
+    
+    function isChosenLevel() {
+        levelEl.addEventListener("change", (event) => {
+            if (event.target.matches('input[type="radio"]')) {
+                startButton.disabled = false;
+            }
+        });
+    }
+    isChosenLevel();
 
-      if (level === "1") {
-        gameLevelEl.innerHTML =
-          '<a href="index.html">level 1. (Легкий уровень - 6 карточек (3 пары))</a>';
-      } else if (level === "2") {
-        gameLevelEl.innerHTML =
-          '<a href="index.html">level 2. (Средний уровень - 12 карточек (6 пар))</a>';
-      } else if (level === "3") {
-        gameLevelEl.innerHTML =
-          '<a href="index.html">level 3. (Сложный уровень - 18 карточек (9 пар))</a>';
-      }
-    });
-  }
-  chooseLevel();
+    function chooseLevel() {
+        startButton.addEventListener("click", () => {
+            const startLevel = levelEl.querySelector(
+                'input[type="radio"]:checked'
+            ).value;
+
+            console.log(startLevel);
+            startGame(startLevel);
+        });
+    }
+    chooseLevel();
 };
 
-renderCardGame();
+const createGameCard = (defaultIcon, flippedCardIcon) => {
+  const card = document.createElement("div");
+  card.classList.add("game-card");
 
+  const notFlippedCardI = document.createElement("img");
+  const flippedCardI = document.createElement("img");
 
-const deck = document.getElementById("deck");
-function renderFrontDeck() {
-    const deckCards = `
-      <div class="top">
-        <div class="time">
-          <div class="time-span">
-            <div class="minutes">min</div>
-            <div class="seconds">sec</div>
-          </div>
-          <div class="time-numbers">00.00</div>
-        </div>
-        <button class="restart">Начать заново</button>
-      </div>
-      <div class="cards">
-        <div class="card-front"></div> 
-        <div class="top">
-        <div class="time">
-          <div class="time-span">
-            <div class="minutes">min</div>
-            <div class="seconds">sec</div>
-          </div>
-          <div class="time-numbers">00.00</div>
-        </div>
-        <button class="restart">Начать заново</button>
-      </div>
-        <div class="card-back"></div>
-      </div>`;
-      
-    deck.innerHTML = deckCards;
-  
-    const suits = ['<img src="./static/spades.svg" class="suit">',
-                        '<img src="./static/hearts.svg" class="suit">', 
-                        '<img src="./static/diamonds.svg" class="suit">', 
-                        '<img src="./static/clubs.svg" class="suit">'];
-    const ranks = ['A', 'K', 'Q', 'J', '10', '9', '8', '7', '6'];
+  notFlippedCardI.setAttribute("src", `${defaultIcon}`);
+  flippedCardI.setAttribute("src", `${flippedCardIcon}`);
 
-   
-    let cards = [];
-  
-    for (let i = 0; i < suits.length; i++) {
-      for (let j = 0; j < ranks.length; j++) {
-        let card = {
-          symbol: suits[i],
-          value: ranks[j]
-        };
-        cards.push(card);
-      }
-    }
-  
-    let frontDeckHtml = '<div class="row">';
-    for (let i = 0; i < 36; i++) {
-      frontDeckHtml += `<div class="card ${cards[i].value}">`;
-      
-      frontDeckHtml += `<div class="symbol-top-left"><div>${cards[i].value}</div><div class="block-symbol">${cards[i].symbol}</div></div>
-                     <div class="value-center my-svg">${cards[i].symbol}</div>
-                     <div class="symbol-bottom-right"><div>${cards[i].value}</div><div class="block-symbol">${cards[i].symbol}</div></div>`;
-      frontDeckHtml += `</div>`;
-    }
-    frontDeckHtml += `</div>`;
-    document.querySelector('.card-front').innerHTML = frontDeckHtml;
-  
-    let backDeckHtml = '<div class="row">';
-    for (let i = 0; i < 36; i++) {
-        backDeckHtml += `<div class="card-back"><img src="./static/back.svg"></div>`;
-        }
-    backDeckHtml += `</div>`;
-    document.querySelector('.card-back').innerHTML = backDeckHtml;
+  card.append(flippedCardI, notFlippedCardI);
+
+  return card;
+};
+
+const topGame = document.querySelector(".top");
+const startGame = (startLevel) => {
+    let firstCard = null;
+    let secondCard = null;
+    let clickable = true;
+
+    const restartButton = document.createElement("button");
+    restartButton.textContent = "Начать заново";
+    restartButton.classList.add("restart");
+    const timer = document.createElement("div");
+    timer.textContent = "00.00";
+    timer.classList.add("timer-counting");
+
+    const gameSection = document.querySelector(".deck");
+    const gameTable = document.createElement("div");
+
+    const initCards = generateCardsArray(startLevel);
+    const doubleCards = doubleCardsArray(initCards);
+
+    gameSection.innerHTML = "";
+    topGame.innerHTML = "";
+
+    gameTable.classList.add("game-table");
+
+    shuffle(doubleCards);
+
+    doubleCards.forEach((icon) =>
+        gameTable.append(createGameCard("./static/back.svg", icon))
+    );
+
+    topGame.append(timer, restartButton);
+    gameSection.append(gameTable);
+
+    const cards = document.querySelectorAll(".game-card");
+
+    restartButton.addEventListener("click", renderCardGame);
+
+    
+    cards.forEach((card, index) =>
+        card.addEventListener("click", () => {
+            if (
+                clickable === true &&
+                !card.classList.contains("successfully")
+            ) {
+                card.classList.add("flip");
+
+                if (firstCard === null) {
+                    firstCard = index;
+                } else {
+                    if (index !== firstCard) {
+                        secondCard = index;
+                        clickable = false;
+                    }
+                }
+
+                if (
+                    firstCard !== null &&
+                    secondCard !== null &&
+                    firstCard !== secondCard
+                ) {
+                    if (
+                        cards[firstCard].firstElementChild.src ===
+                        cards[secondCard].firstElementChild.src
+                    ) {
+                      setTimeout(() => {
+                        cards[firstCard].classList.add("successfully");
+                        cards[secondCard].classList.add("successfully");
+
+                            firstCard = null;
+                            secondCard = null;
+                            clickable = true;
+                            alert("Вы победили!");
+                          }, 500);
+                    } else {
+                      alert("Вы проиграли!");
+                      setTimeout(() => {
+                            cards[firstCard].classList.remove("flip");
+                            cards[secondCard].classList.remove("flip");
+
+                            firstCard = null;
+                            secondCard = null;
+                            clickable = true;
+                          }, 500);
+                    }
+                }
+            }
+        })
+    );
+};
+
+const shuffle = (array) => {
+  let currentIndex = array.length,
+      randomIndex;
+
+  while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [array[currentIndex], array[randomIndex]] = [
+          array[randomIndex],
+          array[currentIndex],
+      ];
   }
 
-  renderFrontDeck();
+  return array;
+};
+
+const doubleCardsArray = (array) =>
+  array.reduce((res, current) => res.concat([current, current]), []);
+
+const generateCardsArray = (startLevel) => {
+  const cards = [
+    "./static/Spades A.png",
+    "./static/Spades K.png",
+    "./static/Spades Q.png",
+    "./static/Spades J.png",
+    "./static/Spades 10.png",
+    "./static/Spades 9.png",
+    "./static/Spades 8.png",
+    "./static/Spades 7.png",
+    "./static/Spades 6.png",
+    "./static/Hearts A.png",
+    "./static/Hearts K.png",
+    "./static/Hearts Q.png",
+    "./static/Hearts J.png",
+    "./static/Hearts 10.png",
+    "./static/Hearts 9.png",
+    "./static/Hearts 8.png",
+    "./static/Hearts 7.png",
+    "./static/Hearts 6.png",
+    "./static/Diamonds A.png",
+    "./static/Diamonds K.png",
+    "./static/Diamonds Q.png",
+    "./static/Diamonds J.png",
+    "./static/Diamonds 10.png",
+    "./static/Diamonds 9.png",
+    "./static/Diamonds 8.png",
+    "./static/Diamonds 7.png",
+    "./static/Diamonds 6.png",
+    "./static/Clubs A.png",
+    "./static/Clubs K.png",
+    "./static/Clubs Q.png",
+    "./static/Clubs J.png",
+    "./static/Clubs 10.png",
+    "./static/Clubs 9.png",
+    "./static/Clubs 8.png",
+    "./static/Clubs 7.png",
+    "./static/Clubs 6.png",
+  ];
+  cards.sort(() => Math.random() - 0.5);
+  
+  
+    
+  switch (startLevel) {
+    case "1":
+      cards.length = 3;
+      return cards;
+      
+    case "2":
+      cards.length = 6;
+      return cards;
+    case "3":
+      cards.length = 9;
+      return cards;
+    default:
+        break;
+  }
+} 
+;
+
+const cardsApp = () => {
+  renderCardGame();
+};
+
+cardsApp();
+
 })();
 
 /******/ })()
