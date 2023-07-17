bed73ee9db5446be8e23c8467457988d18d67a31
TaTia <tatyanakoryachkina@yandex.ru>
2023-07-17 21:14:11 +0300

    did 6th hw

--- shuffle.js
+++
@@ -1,3 +0,0 @@
-function shuffleCards (cards) {
-    return cards.sort(() => Math.random() - 0.5);
-  }
--- shuffle.test.js
+++ shuffle.test.js
@@ -1,13 +1,21 @@
-const { shuffleCards } = require("./shuffle");
-const { expect, it, describe } = require ("@jest/globals");
+function shuffleCards(cards) {
+  const shuffledCards = [...cards];
+  for (let i = shuffledCards.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledCards[i], shuffledCards[j]] = [shuffledCards[j], shuffledCards[i]];
+  }
+  return shuffledCards;
+}
+
+const { expect, it, describe } = require("@jest/globals");
 
 describe('shuffleCards', () => {
-    it('should shuffle the cards array', () => {
-      const cards = ['A', 'B', 'C', 'D', 'E'];
-      const shuffledCards = shuffleCards(cards);
-  
-      expect(shuffledCards).not.toEqual(cards);
-      expect(shuffledCards).toHaveLength(cards.length);
-      expect([...new Set(shuffledCards)]).toEqual(cards);
-    });
+  it('should shuffle the cards array', () => {
+    const cards = ['A', 'B', 'C', 'D', 'E'];
+    const shuffledCards = shuffleCards(cards);
+
+    expect(shuffledCards).not.toEqual(cards);
+    expect(shuffledCards).toHaveLength(cards.length);
+    expect(shuffledCards).toEqual(expect.arrayContaining(cards));
   });
+});
